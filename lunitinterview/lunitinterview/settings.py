5fd6c9e4a025beb92b0cf9cf0a72229281ba925b
sparcsJara <jara0701@gmail.com>
2021-01-22 04:17:43 +0900

    Point Model & Contour Model design

--- lunitinterview/lunitinterview/settings.py
+++ lunitinterview/lunitinterview/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'point.apps.PointConfig',
     'rest_framework',
 ]
 
--- lunitinterview/point/models.py
+++ lunitinterview/point/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from enum import Enum
+
 
 # Create your models here.
+
+class Coordinate(models.Model):
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+
+    class Meta:
+        abstract = True
+
+class Point(Coordinate):
+    class Meta:
+        constraints = [
+            models.CheckConstraint(check=models.Q(longitude__gte=-180), name='longitude_gte_minus_180'),
+            models.CheckConstraint(check=models.Q(longitude__lte=180), name='longitude_lte_180'),
+            models.CheckConstraint(check=models.Q(latitude__gte=-90), name='latitude_gte_minus_90'),
+            models.CheckConstraint(check=models.Q(latitude__lte=90), name='latitude_lte_90'),
+        ]
+
+class Contour(models.Model):
+    pass
+
+class ContourPoint(Coordinate):
+    contour = models.ForeignKey(
+        Contour,
+        related_name='coordinate',
+        on_delete=models.CASCADE
+    )
+    class Meta:
+        constraints = [
+            models.CheckConstraint(check=models.Q(longitude__gte=-180), name='cp_longitude_gte_minus_180'),
+            models.CheckConstraint(check=models.Q(longitude__lte=180), name='cp_longitude_lte_180'),
+            models.CheckConstraint(check=models.Q(latitude__gte=-90), name='cp_latitude_gte_minus_90'),
+            models.CheckConstraint(check=models.Q(latitude__lte=90), name='cp_latitude_lte_90'),
+        ]
+
+
+
