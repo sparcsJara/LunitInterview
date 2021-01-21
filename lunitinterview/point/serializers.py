be3b3961f7789564b6dff26a6b36b38f4ca8b49a
sparcsJara <jara0701@gmail.com>
2021-01-22 04:22:07 +0900

    Model들의 Serializer 추가

---
+++ lunitinterview/point/serializers.py
@@ -0,0 +1,27 @@
+from lunitinterview.point.models import *
+from rest_framework import serializers
+from django.conf import settings
+
+
+class PointSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Point
+        fields = '__all__'
+
+
+class ContourPointSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContourPoint
+        fields = '__all__'
+
+
+class ContourSerializer(serializers.ModelSerializer):
+    coordinates = ContourPointSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Contour
+        fields = (
+            'id',
+            'stories'
+        )
+        # auto_now_add나 auto_now가 true이면 read_only_fields여야 함.
