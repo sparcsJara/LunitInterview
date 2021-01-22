a9045d0eb92a8ffdd0cd0dc80d0aa35ebe71a6a4
sparcsJara <jara0701@gmail.com>
2021-01-23 03:04:29 +0900

    implement post /points

--- lunitinterview/point/backends.py
+++ lunitinterview/point/backends.py
@@ -14,8 +14,16 @@ def convert_point_to_GEOJson(serializer_data):
     data = {}
     GEOJson_data = {}
     coordinate = [serializer_data["longitude"], serializer_data["latitude"]]
-    GEOJson_data["type"] = GEOJSsonType.point
+    GEOJson_data["type"] = GEOJSsonType.point.value
     GEOJson_data["coordinates"] = coordinate
     data["id"] = serializer_data["id"]
     data["data"] = GEOJson_data
     return data
+
+
+def extract_data_from_GEOJson(data):
+    if data["data"]["type"] == GEOJSsonType.point.value:
+        point_data = {}
+        point_data["longitude"] = data["data"]["coordinates"][0]
+        point_data["latitude"] = data["data"]["coordinates"][1]
+        return point_data
--- lunitinterview/point/views.py
+++ lunitinterview/point/views.py
@@ -2,16 +2,13 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import viewsets
 
-from point.backends import convert_to_GEOJson
+from point.backends import convert_to_GEOJson, extract_data_from_GEOJson
 from point.models import *
 from point.serializers import *
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import action
-
-
-# Create your views here.
 
 class PointViewSet(viewsets.ModelViewSet):
     queryset = Point.objects.all()
@@ -22,7 +19,6 @@ class PointViewSet(viewsets.ModelViewSet):
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
-            data = serializer.data
             return Response(convert_to_GEOJson(serializer.data,many=True))
         serializer = self.get_serializer(queryset, many=True)
         return Response(convert_to_GEOJson(serializer.data))
@@ -31,6 +27,15 @@ class PointViewSet(viewsets.ModelViewSet):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
         return Response(convert_to_GEOJson(serializer.data))
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=extract_data_from_GEOJson(request.data))
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        data = convert_to_GEOJson(serializer.data)
+        data["id"] = serializer.data["id"]
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
 
 class ContourViewSet(viewsets.ModelViewSet):
     queryset = Contour.objects.all()
