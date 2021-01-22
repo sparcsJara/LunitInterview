75b32ec4afae9b3d1e0d91f85f76f2d7773be173
sparcsJara <jara0701@gmail.com>
2021-01-23 00:12:15 +0900

    pagination added for list api

--- lunitinterview/lunitinterview/settings.py
+++ lunitinterview/lunitinterview/settings.py
@@ -101,6 +101,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
