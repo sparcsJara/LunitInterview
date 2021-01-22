67b9f03337858f4ae841c7f3a195174cf1a22939
sparcsJara <jara0701@gmail.com>
2021-01-23 03:20:17 +0900

    implement patch /points

--- lunitinterview/point/views.py
+++ lunitinterview/point/views.py
@@ -37,6 +37,21 @@ class PointViewSet(viewsets.ModelViewSet):
         data["id"] = serializer.data["id"]
         return Response(data, status=status.HTTP_201_CREATED, headers=headers)
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=extract_data_from_GEOJson(request.data), partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+
 class ContourViewSet(viewsets.ModelViewSet):
     queryset = Contour.objects.all()
     serializer_class = ContourSerializer
