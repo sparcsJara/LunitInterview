bbfbd6c9ea7238f94b2025314e7b6b455a592765
sparcsJara <jara0701@gmail.com>
2021-01-24 08:03:56 +0900

    contour curd test added

--- lunitinterview/point/tests.py
+++ lunitinterview/point/tests.py
@@ -1,12 +1,6 @@
-import base64
-
-from django.test import TestCase, Client
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase, APIClient
-
-from point.models import Point
-
 
 class PointTests(APITestCase):
 
@@ -258,33 +252,6 @@ class PointTests(APITestCase):
         response = self.client.get(redirect_url, format='json')
         self.assertEqual(response.data, data)
 
-    def test_inside_api(self):
-
-        data = {"points":[{
-            "id": 1,
-            "data": {
-                "type": "Point",
-                "coordinates": [0,0]
-            }},
-            {
-                "id": 2,
-                "data": {
-                    "type": "Point",
-                    "coordinates": [10, 0]
-                }},
-            {
-                "id": 3,
-                "data": {
-                    "type": "Point",
-                    "coordinates": [20, 0]
-                }}
-        ]}
-        url = "/points?contour=1"
-        redirect_url = '/points/?contour=1'
-        response = self.client.get(url, format='json')
-        self.assertRedirects(response, redirect_url, status_code=301, target_status_code=200)
-        response = self.client.get(redirect_url, format='json')
-        self.assertEqual(response.data, data)
 
     def test_inside_api_validation_error(self):
         url = "/points?contour=2"
@@ -293,3 +260,117 @@ class PointTests(APITestCase):
         self.assertRedirects(response, redirect_url, status_code=301, target_status_code=400)
         response = self.client.get(redirect_url, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+class ContourTests(APITestCase):
+
+
+    def setUp(self) -> None:
+        url = reverse('contour-list')
+        data1 = {
+                "data":
+                    {
+                        "type": "Polygon",
+                        "coordinates": [
+                            [0, 0.0], [10.0, 0.0], [10.0, 20.0], [0, 20.0]
+                        ]
+                    }
+
+            }
+
+        self.client.post(url, data1, format='json')
+        data2 = {
+                "data":
+                    {
+                        "type": "Polygon",
+                        "coordinates": [
+                            [20.0, 0.0], [5.0, 5.0], [20.0, 10.0], [5, 15], [20, 20], [30, 10]
+                        ]
+                    }
+
+            }
+
+        self.client.post(url, data2, format='json')
+
+
+
+    def test_create_contour(self):
+        """
+        Ensure we can create a contour object.
+        """
+        url = reverse('contour-list')
+        data = {
+                "data":
+                    {
+                        "type": "Polygon",
+                        "coordinates": [
+                            [0, 0.0], [40.0, 0.0], [20.0, 50.0]
+                        ]
+                    }
+
+            }
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_retrieve_contours(self):
+        """
+        Ensure we can retrive contours
+        """
+        url = reverse('contour-list')
+
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_single_contour(self):
+        """
+        Ensure we can retrieve a single point.
+        """
+        url = "/contours/1/"
+        data = {
+            "id": 1,
+            "data": {
+                "type": "Polygon",
+                "coordinates": [
+                    [0, 0.0], [10.0, 0.0], [10.0, 20.0], [0, 20.0]
+                ]
+            }
+        }
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, data)
+
+    def test_update_contour(self):
+        """
+        Ensure we can update a contour object.
+        """
+        url = "/contours/1/"
+        data = {"data":{
+            "type": "Polygon",
+            "coordinates": [
+                [0, 0.0], [10.0, 0.0], [10.0, 20.0], [0, 40.0]
+            ]
+        }}
+
+
+        expected_data = {
+            "id": 1,
+            "data": {
+                "type": "Polygon",
+            "coordinates": [
+                [0, 0.0], [10.0, 0.0], [10.0, 20.0], [0, 40.0]
+            ]
+            }
+        }
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, expected_data)
+
+    def test_delete_single_point(self):
+        """
+        Ensure we can delete a single point.
+        """
+        url = "/contours/1/"
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
